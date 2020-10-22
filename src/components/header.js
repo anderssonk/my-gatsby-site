f71747ebbd80467b86231628f75f102d870d1ab0
Kristina Andersson <kristina.andersson@mail.ru>
2020-10-22 22:29:54 +0200

    Style nav-bar in Header

--- src/components/header.js
+++ src/components/header.js
@@ -4,27 +4,53 @@ import headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to='/'>
+          Kristina Andersson
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link className={headerStyles.link} to='/'>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to='/'
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to='/about'
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to='/blog'>Blog</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to='/blog'
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to='/contact'>Contact</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to='/contact'
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 }
 
