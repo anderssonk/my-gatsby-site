23b2dad77f61ad1ebe2a40d6da0a0c62214966af
Kristina Andersson <kristina.andersson@mail.ru>
2020-10-22 16:41:36 +0200

    Add components AboutPage, ContactPage

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>
+        I am currently pursuing my Master of Science in Computer Science and
+        Engineering / Interactive Media Technology at KTH Royal Institute of
+        Technology, Stockholm.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        The best way to contact me is by email, kristina-andersson@outlook.com,
+        or by giving me a call! My number is +46(0)76 576 68 02
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
