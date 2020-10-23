b70bc59b75f572489e644fecc658f221a4a7bf35
Kristina Andersson <kristina.andersson@mail.ru>
2020-10-23 13:01:20 +0200

    Add dynamic blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  const posts = data.allMarkdownRemark.edges
+  const listItems = posts.map(post => (
+    <li key={post.node.frontmatter.title}>
+      <h1>{post.node.frontmatter.title}</h1>
+      <p>{post.node.frontmatter.date}</p>
+    </li>
+  ))
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>{listItems}</ol>
     </Layout>
   )
 }
