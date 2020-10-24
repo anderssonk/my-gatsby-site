a4e4def3b57efc0f6e6555d1c266e308e7d3be56
Kristina Andersson <kristina.andersson@mail.ru>
2020-10-24 17:27:05 +0200

    Update gatsby-node config file for blog post page generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,3 +15,34 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve('./src/templates/blog.js')
+  // Get absolute path to template
+
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `) // res (response) awaits the promise from graphql()
+  // Get markdown data
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  }) // Create new page
+}
